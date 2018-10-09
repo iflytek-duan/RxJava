4baac0ac9fe8f524a15f178b843644927e694ddc
jgduan <crazy.zihao@gmail.com>
2018-10-09 14:54:25 +0800

    1.Add observable#timer simple code; 2.Add observable#interval simple code.

--- app/src/main/java/com/zihao/rxjava/MainActivity.java
+++ app/src/main/java/com/zihao/rxjava/MainActivity.java
@@ -10,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
@@ -17,6 +18,7 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private Disposable mDisposableInterval;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,14 +55,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Observable.timer(2, TimeUnit.SECONDS)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())// timer 默认在新线程，所以需要切换回主线程
-                .subscribe(new Consumer<Long>() {
-                    @Override
-                    public void accept(Long aLong) throws Exception {
-                        Log.e(TAG, "timer :" + aLong + " at " + System.currentTimeMillis());
-                    }
-                });
+        // time计时器
+//        Observable.timer(2, TimeUnit.SECONDS)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())// timer 默认在新线程，所以需要切换回主线程
+//                .subscribe(new Consumer<Long>() {
+//                    @Override
+//                    public void accept(Long aLong) throws Exception {
+//                        Log.e(TAG, "timer :" + aLong + " at " + System.currentTimeMillis());
+//                    }
+//                });
+
+        // interval间隔任务
+        if (mDisposableInterval != null && !mDisposableInterval.isDisposed()) {
+            mDisposableInterval.dispose();
+            mDisposableInterval = null;
+        } else {
+            mDisposableInterval = Observable.interval(0, 2, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<Long>() {
+                        @Override
+                        public void accept(Long aLong) throws Exception {
+                            Log.e(TAG, "timer :" + aLong + " at " + System.currentTimeMillis());
+                        }
+                    });
+        }
     }
 }
